88881ab2509cbdeed70df2e060286e343d9984ad
Kuba <kub.szy2002@gmail.com>
2023-06-28 15:40:32 +0200

    flaga init

--- skrypt.sh
+++ skrypt.sh
@@ -21,4 +21,15 @@ elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "--logs, -l <liczba>: Tworzy <liczba> plików logx.txt z informacjami."
     echo "--help, -h: Wyświetla dostępne opcje skryptu."
     exit 0
+elif [[ "$1" == "--init" || "$1" == "-i" ]]; then
+    git clone https://github.com/Glosin/bash.git skrypt
+    repo_path=$(pwd)/skrypt
+    if [[ ":$PATH:" != *":$repo_path:"* ]]; then
+        echo "Dodawanie ścieżki repozytorium do zmiennej środowiskowej PATH"
+        echo "export PATH=\"\$PATH:$repo_path\"" >> ~/.bashrc
+        source ~/.bashrc
+    fi
+
+    echo "Repozytorium zostało sklonowane, a ścieżka została dodana do zmiennej środowiskowej PATH."
+    exit 0
 fi
