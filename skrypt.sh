6bb0234d735710669a2d0206cc7e1478f2a75dc4
Kuba <kub.szy2002@gmail.com>
2023-06-28 15:12:09 +0200

    Obsługa argumentów dla plików log

--- skrypt.sh
+++ skrypt.sh
@@ -3,8 +3,12 @@
 if [[ "$1" == "--date" ]]; then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
-elif [[ "$1" == "--logs" ]]; then
-    for ((i=1; i<=100; i++)); do
+elif [[ "$1" == "--logs" ]] then
+    num_files=$2
+    if [[ -z "$num_files" ]]; then
+        num_files=100
+    fi
+    for ((i=1; i<=num_files; i++)); do
         filename="log${i}.txt"
         echo "Nazwa pliku: $filename" > "$filename"
         echo "Nazwa skryptu: skrypt.sh" >> "$filename"
