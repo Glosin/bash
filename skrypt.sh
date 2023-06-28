a4a773855e8d867ddfaeeb37f9c32d168ec4820b
Kuba <kub.szy2002@gmail.com>
2023-06-28 15:34:50 +0200

    Dodanie flag z pojedyńczym -

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
-elif [[ "$1" == "--logs" ]] then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]] then
     num_files=$2
     if [[ -z "$num_files" ]]; then
         num_files=100
@@ -15,10 +15,10 @@ elif [[ "$1" == "--logs" ]] then
         current_date=$(date +"%Y-%m-%d")
         echo "Data: $current_date" >> "$filename"
     done
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Dostępne opcje skryptu:"
-    echo "--date: Wyświetla dzisiejszą date."
-    echo "--logs <liczba> : Tworzy <liczba> plików logx.txt z informacjami."
-    echo "--help : Wyświetla dostępne opcje skryptu."
+    echo "--date, -d: Wyświetla dzisiejszą date."
+    echo "--logs, -l <liczba>: Tworzy <liczba> plików logx.txt z informacjami."
+    echo "--help, -h: Wyświetla dostępne opcje skryptu."
     exit 0
 fi
