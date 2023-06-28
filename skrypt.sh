2498f45a7f01727a26c3c239fc8a0d884eadad42
Kuba <kub.szy2002@gmail.com>
2023-06-28 15:04:34 +0200

    Zapisanie 100 plików z datą, nazwą pliku, nazwą skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -3,4 +3,12 @@
 if [[ "$1" == "--date" ]]; then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
+elif [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+        current_date=$(date +"%Y-%m-%d")
+        echo "Data: $current_date" >> "$filename"
+    done
 fi
